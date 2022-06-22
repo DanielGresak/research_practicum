a81c5708fbffaf882d21fcc2d627d79c85171c7b
zhang yating <zhangyatingfra@sina.com>
2022-06-22 13:23:50 +0100

    correct the errors

--- core/core/views.py
+++ core/core/views.py
@@ -3,8 +3,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
-<<<<<<< HEAD:core/views.py
-=======
-
-
->>>>>>> refs/remotes/origin/developement:core/core/views.py
