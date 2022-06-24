d48d5546efe3474d6f82a2d0445e744a18d58519
DanielGresak <80643624+DanielGresak@users.noreply.github.com>
2022-06-24 10:38:29 +0100

    settings changed

--- core/core/settings.py
+++ core/core/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 # STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = '/vol/web/static'
 MEDIA_ROOT = '/vol/web/media'
