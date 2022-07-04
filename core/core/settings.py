edea7c64e504cc56ba79ccbca739a90e9ec311ef
DanielGresak <80643624+DanielGresak@users.noreply.github.com>
2022-07-04 09:23:41 +0100

    not too sure, probably a space

--- core/core/settings.py
+++ core/core/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-# STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = '/vol/web/static'
 MEDIA_ROOT = '/vol/web/media'
 
