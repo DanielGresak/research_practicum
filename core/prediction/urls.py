04d3ec2bf5a4f3d9bab0abd5275fb502d05c8cf6
Jorg8383 <jorg.striebel@ucdconnect.ie>
2022-07-22 12:52:34 +0100

    Eliminate PEP8 compliance  issues before implementing random forest

--- core/prediction/urls.py
+++ core/prediction/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, register_converter
 from . import views
 
 urlpatterns = [
-    path("prediction/<str:line_id>/<str:direction>/<int:traveltime>/", views.predict_travel_time, name="api_prediction")
+    path("prediction/<str:line_id>/<str:direction>/<int:traveltime>/",\
+        views.predict_travel_time, name="api_prediction")
 ]
