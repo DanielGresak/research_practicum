ffa2ce27cdd8da6d775344f6c59c2f701a0a8449
DanielGresak <80643624+DanielGresak@users.noreply.github.com>
2022-07-27 18:53:32 +0100

    Added float instead of int for co2

--- core/carbonCalculator/views.py
+++ core/carbonCalculator/views.py
@@ -25,7 +25,7 @@ transport land work.
 def CarbonCalculator(request):
     if request.method == 'POST':
         bus_distance = int(request.POST.get('bus_distance', False)) / 1000
-        dist = int(request.POST.get('driving_distance', False))
+        dist = float(request.POST.get('driving_distance', False))
         driving_distance = dist / 1000
         saved_emissions = calculate_emissions(bus_distance, driving_distance)
         if request.user.is_authenticated:
