69450deec4601d4010b27e3dc8dcc5891a098561
DanielGresak <80643624+DanielGresak@users.noreply.github.com>
2022-07-28 17:47:33 +0100

    fixed carbon to float

--- core/users/models.py
+++ core/users/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    emissions = models.IntegerField(default=0)
+    emissions = models.FloatField(default=0.0)
     notifications = models.BooleanField(default=False)
     notification_delay = models.IntegerField(default=5)
     age = models.TextField(default="adult")
