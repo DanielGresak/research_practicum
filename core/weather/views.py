626b6dfc5ec04b0428529f3d3a6697010f0d9205
Jorg8383 <jorg.striebel@ucdconnect.ie>
2022-07-29 14:30:11 +0100

    Make views.py compliant with PEP8

--- core/weather/views.py
+++ core/weather/views.py
@@ -13,24 +13,21 @@ from itertools import chain
 
 def weather_data_json(request):
     """Weather forecast API that returns the forecast as JSON"""
+
     try:
         # Get a QuerySets of dictionaries according to the provided values
-        forecast_query_set = Forecast.objects.all()[:40].values("dt", "dt_txt",
-                                                           "temp", "temp_min",  # noqa
-                                                           "temp_max", "weather_main",  # noqa
-                                                           "weather_icon", "pop")  # noqa
+        forecast_query_set = Forecast.objects.all()[:40].values(
+                "dt", "dt_txt", "temp", "temp_min", "temp_max",
+                "weather_main", "weather_icon", "pop")
         # Convert the QuerySet to a list of dictionaries
         forecast_list = list(forecast_query_set)
         # Get last weather object of the current weather table,
-        # which is also the latest
         # last_weather_obj = CurrentWeather.objects.filter(dt__gt=0).last()
         last_weather_obj = CurrentWeather.objects.last()
-        # Query current weather data by filtering for timestamp value
-        # We'll be receiving a QuerySet that contains only one object.
-        # However, we require a QuerySet so that we can
-        # actually chain the forecast
-        # and current weather information
-        weather_query_set = CurrentWeather.objects.filter(id=last_weather_obj.id).values()  # noqa
+        # We require a QuerySet so that we can actually chain
+        # the forecast and current weather information
+        weather_query_set = CurrentWeather.objects.filter(
+                id=last_weather_obj.id).values()
         # Chain both QuerySets - forecast and current weather information
         result_list = list(chain(forecast_list, weather_query_set))
         result_data = json.dumps(result_list)
@@ -38,6 +35,5 @@ def weather_data_json(request):
     except Exception as e:
         # Instead of using "HttpResponseNotFound", use Http404 instead
         # To use this function a 404.html must be globally defined
-        # https://docs.djangoproject.com/en/4.0/topics/http/views/#the-http404-exception
         print(e)
         raise Http404()
